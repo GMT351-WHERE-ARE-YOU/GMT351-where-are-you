07c5a2f3cff3d5230d494fe032c64e5fd8392dce
Melih Altay <melihaltay2017@gmail.com>
2021-01-18 15:30:12 +0300

    Update findlocation.py

--- findlocation.py
+++ findlocation.py
@@ -4,25 +4,38 @@
 #pip install requests
 
 
+import time
 import requests
+import schedule
 
-r = requests.get('https://get.geojs.io/')
-
-ip_request = requests.get('https://get.geojs.io/v1/ip.json')
-ippAdd = ip_request.json()["ip"]
-print(ippAdd)
+def executeSomething():
 
 
-url = 'https://get.geojs.io/v1/ip/geo/' + ippAdd + '.json'
-geo_request = requests.get(url)
-geo_data = geo_request.json()
-#print(geo_data)
+    r = requests.get('https://get.geojs.io/')
+
+    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+    ippAdd = ip_request.json()["ip"]
+    print(ippAdd)
+
+    url = 'https://get.geojs.io/v1/ip/geo/' + ippAdd + '.json'
+    geo_request = requests.get(url)
+    geo_data = geo_request.json()
+    # print(geo_data)
+
+    print(geo_data['latitude'])
+    print(geo_data['longitude'])
+
+    print(geo_data['city'])
+    print(geo_data['region'])
+    print(geo_data['country'])
+    print(geo_data['timezone'])
 
 
-print(geo_data['latitude'])
-print(geo_data['longitude'])
+schedule.every(10).seconds.do(executeSomething)#This code run every 10 seconds
+#schedule.every().hour.do(executeSomething())
 
-print(geo_data['city'])
-print(geo_data['region'])
-print(geo_data['country'])
-print(geo_data['timezone'])
+
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
