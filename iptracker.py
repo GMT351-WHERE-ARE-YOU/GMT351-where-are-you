59a439014a53800108b0992272f9a26a9d191038
Melih Altay <melihaltay2017@gmail.com>
2021-01-19 17:29:50 +0300

    Update iptracker.py

--- iptracker.py
+++ iptracker.py
@@ -1,23 +1,35 @@
 import os
 import urllib.request
 import json
+import time
+import schedule
+
+def executeSomething():
+
+    while True:
+        ip = input("What is your target ip : ")
+        url = "http://ip-api.com/json/"
+        response = urllib.request.urlopen(url + ip)
+        data = response.read()
+        values = json.loads(data)
+
+        print("IP:" + values['query'])
+        print("City:" + values["city"])
+        print("Country:" + values['country'])
+        print("Region:" + values['region'])
+        print("Time zone:" + values['timezone'])
+        print("Latitude:" + str(values["lat"]))
+        print("Longitude:" + str(values["lon"]))
+        print("Isp:" + values['isp'])
+        print("RegionName:" + values['regionName'])
+
+        break
 
 
-while True:
-    ip = input("What is your target ip : ")
-    url = "http://ip-api.com/json/"
-    response = urllib.request.urlopen(url + ip)
-    data = response.read()
-    values = json.loads(data)
+schedule.every(1).seconds.do(executeSomething)#This code run every 10 seconds
+#schedule.every().hour.do(executeSomething())
 
-    print("IP:" + values['query'])
-    print("City:" + values["city"])
-    print("Country:" + values['country'])
-    print("Region:" + values['region'])
-    print("Time zone:" + values['timezone'])
-    print("Latitude:" + str(values["lat"]))
-    print("Longitude:" + str(values["lon"]))
-    print("Isp:" + values['isp'])
-    print("RegionName:" + values['regionName'])
 
-    break
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
